d0e01a70996e9bbb78b9e9aca4b1b1ecb7a9385a
bam <devbam515@gmail.com>
2023-05-01 20:51:37 +0900

    admin -ing

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,23 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Notice;
+use App\Models\Plant;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     // dashboard index
     public function index(Request $request) {
-        return view('admin.dashboard.index');
+        // 회원 수
+        $memberCount = User::count();
+        // 화분 수
+        $plantCount = Plant::count();
+        // 공지사항 수
+        $noticeCount = Notice::count();
+
+        return view('admin.dashboard.index',
+            compact('memberCount', 'plantCount', 'noticeCount'));
     }
 }
---
+++ routes/admin/plant.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\PlantController;
+
+Route::middleware('admin')->group(function () {
+    // 화단 관리
+    Route::get('/', [PlantController::class, 'index'])->name('admin.plant.index');
+
+    // 상세보기
+    Route::get('/detail/{plant}', [PlantController::class, 'show'])->name('admin.plant.show');
+
+    // 삭제
+    Route::delete('/{plant}', [PlantController::class, 'delete'])->name('admin.plant.delete');
+});
